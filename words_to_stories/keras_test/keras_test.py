afb96dd8f2a2cbaf3f1d19744c9d2bb24a559d3f
Saksham Shah <saksham101@gmail.com>
2022-02-06 19:34:33 +0000

    Attempt Keras test
    Didn't work :(
    Just outputs loads of spaces

--- words_to_stories/keras_test/keras_test.py
+++ words_to_stories/keras_test/keras_test.py
@@ -2,7 +2,7 @@
 
 # Keras is an easier to use API for TensorFlow
 
-# Use 'pip3 install' to install  to run this
+# Use 'pip3 install' to install any modules that aren't found to run this
 
 # frankenstein.txt has the first three chapters
 
@@ -49,3 +49,73 @@ seq_length = 100
 x_data = []
 y_data = []
 
+# Loop through inputs, start at the beginning and go until we hit
+# the final character we can create a sequence out of
+for i in range(0, input_len - seq_length, 1):
+    # Define input and output sequences
+    # Input is the sequence of characters starting from the current one
+    in_seq = processed_inputs[i:i + seq_length]
+
+    # Output is the character right aftter the input
+    out_seq = processed_inputs[i + seq_length]
+
+    # Convert list of characters to integers
+    # Add the values to our lists
+    x_data.append([char_to_num[char] for char in in_seq])
+    y_data.append(char_to_num[out_seq])
+
+n_patterns = len(x_data)
+print("Total patterns:", n_patterns)
+
+X = numpy.reshape(x_data, (n_patterns, seq_length, 1))
+X = X/float(vocab_len)
+
+y = np_utils.to_categorical(y_data)
+
+model = Sequential()
+model.add(LSTM(256, input_shape = (X.shape[1], X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# # Set checkpoint to save model to
+# filepath = "model_weights_saved.hdf5"
+# checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+# desired_callbacks = [checkpoint]
+
+# # Train model
+# print("Training model")
+# model.fit(X, y, epochs=4, batch_size=256, callbacks=desired_callbacks)
+
+# Load model
+filename = "model_weights_saved.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+num_to_char = dict((i, c) for i, c in enumerate(chars))
+
+start = numpy.random.randint(0, len(x_data) - 1)
+pattern = x_data[start]
+print("Random Seed:")
+print("\"", ''.join([num_to_char[value] for value in pattern]), "\"")
+
+output = []
+
+for i in range(1000):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(vocab_len)
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = num_to_char[index]
+
+    # sys.stdout.write(result)
+    output.append(result)
+    print(result)
+
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
