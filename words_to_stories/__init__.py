7b4ce9cabdd98f8941acb7bfe90830682c37e924
Saksham Shah <saksham101@gmail.com>
2022-02-23 23:14:51 +0000

    Move GPT2 code into __init__.py

--- words_to_stories/__init__.py
+++ words_to_stories/__init__.py
@@ -1,3 +1,17 @@
+import create_stories
+from os import path
+
+def load_model():
+    if not path.exists("models"):
+        print("GPT2 model not yet downloaded. Downloading into 'models' folder.")
+        create_stories.download_model()
+    
+    print("Loading GPT2 model")
+    return create_stories.load_model()
+
 def convert(wordtriple):
-    sentence = "This sentence contains the words \"" + wordtriple[0] + ", " + wordtriple[1] + ", " + wordtriple[2] + "\"."
+    sess = load_model()
+    sentence = create_stories.generateWithKeywords(sess, "Once upon a time, ", wordtriple)
+    print(str(sentence))
+
     return sentence
