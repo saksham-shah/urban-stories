241171614f52dc560e296d4cac52d644aedd5b21
Qiu Yuan <qiuyuan463@gmail.com>
2022-02-24 11:00:40 +0000

    Add files via upload

--- locations_to_words/__init__.py
+++ locations_to_words/__init__.py
@@ -1,9 +1,5 @@
-import what3words
+import extract_word
 
-def w3w_convert(lat, lon):
-    geocoder = what3words.Geocoder("L5311VV5")
-    words = geocoder.convert_to_3wa(what3words.Coordinates(lat,lon))['words'].split(".")
-    return words
 
 def similar_word_convert(lat, lon):
-    return ["place", "holder", "words"]
+    return extract_word.conclude_words(lat, lon)
