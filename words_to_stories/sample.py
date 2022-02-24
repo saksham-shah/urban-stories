dbbb76ba6ae1f6e5bdbed258871d8379b8b1cb0f
miagittens <98815062+miagittens@users.noreply.github.com>
2022-02-24 17:17:37 +0000

    Update sample.py
    probabilities and token sequence are now returned by sample_sequence as one tensor that can be unpacked by the caller
    commented whats happening and cleaned up all the print statements.

--- words_to_stories/sample.py
+++ words_to_stories/sample.py
@@ -17,6 +17,7 @@ def top_k_logits(logits, k):
             tf.ones_like(logits, dtype=logits.dtype) * -1e10,
             logits,
         )
+
     return tf.cond(
         pred=tf.equal(k, 0),
         true_fn=lambda: logits,
@@ -30,7 +31,7 @@ def top_p_logits(logits, p):
         probs_sort = tf.nn.softmax(logits_sort)
         probs_sums = tf.cumsum(probs_sort, axis=1, exclusive=True)
         logits_masked = tf.compat.v1.where(probs_sums < p, logits_sort, tf.ones_like(
-            logits_sort)*1000)  # [batchsize, vocab]
+            logits_sort) * 1000)  # [batchsize, vocab]
         min_logits = tf.reduce_min(input_tensor=logits_masked, axis=1, keepdims=True)  # [batchsize, 1]
 
         return tf.compat.v1.where(
@@ -40,18 +41,19 @@ def top_p_logits(logits, p):
         )
 
 
-
-
-def sample_sequence(target,*, hparams, length, start_token=None,
+def sample_sequence(target, *, hparams, length, start_token=None,
                     batch_size=None, context=None, temperature=1,
                     top_k=0, top_p=0.0):
+
     if start_token is None:
         assert context is not None, 'Specify exactly one of start_token and context!'
     else:
         assert context is None, 'Specify exactly one of start_token and context!'
         context = tf.fill([batch_size, 1], start_token)
 
+
     def step(hparams, tokens, past=None):
+
         lm_output = model.model(hparams=hparams, X=tokens,
                                 past=past, reuse=tf.compat.v1.AUTO_REUSE)
 
@@ -70,21 +72,17 @@ def sample_sequence(target,*, hparams, length, start_token=None,
         # TODO: Would be slightly faster if we called step on the entire context,
         # rather than leaving the last token transformer calculation to the while loop.
         context_output = step(hparams, context[:, :-1])
-
-        prob = tf.constant([np.NINF])
-        #print(target,"pre")
-        #target = tf.constant(1169)
-        #print(target,"post")
-        def body(past, prev, output,prob,target):
+        #pad out the probabilities tensor so it is same size as the tokens list
+        #give the target word NINF probability so the index to truncate at will never be part of the prompt
+        prob = tf.fill(tf.shape(context), np.NINF)
+        def body(past, prev, output, prob, target):
             next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
             logits = next_outputs['logits'][:, -1, :] / tf.cast(temperature, tf.float32)
+
+            #get the probability of the target word and put it in a singleton list so it can be appended to the
+            #full tensor
             target_value = logits[0][target]
-            #print(target_value,"target")
-            target_value = tf.reshape(target_value,[1])
-            #print(target_value)
-            #print(prob,"prob")
-            #print(tf.concat([prob, target_value], axis=-1))
-            #print("target val and probs so far")
+            target_value = tf.reshape(target_value, [1, 1])
 
             if top_p > 0.0:
                 logits = top_p_logits(logits, p=top_p)
@@ -96,14 +94,14 @@ def sample_sequence(target,*, hparams, length, start_token=None,
                 tf.concat([past, next_outputs['presents']], axis=-2),
                 tf.squeeze(samples, axis=[1]),
                 tf.concat([output, samples], axis=1),
-                tf.concat([prob, target_value], axis=-1),
+                tf.concat([prob, target_value], axis=1),
                 target,
             ]
 
         def cond(*args):
             return True
 
-        _, _, tokens,prob,target= tf.nest.map_structure(
+        _, _, tokens, prob, target = tf.nest.map_structure(
             tf.stop_gradient,
             tf.while_loop(
                 cond=cond,
@@ -113,6 +111,7 @@ def sample_sequence(target,*, hparams, length, start_token=None,
                     context_output['presents'],
                     context[:, -1],
                     context,
+                    #pass the list of probabilities at each index and the target word into each iteration of the loop
                     prob,
                     target,
                 ],
@@ -120,10 +119,14 @@ def sample_sequence(target,*, hparams, length, start_token=None,
                     tf.TensorShape(model.past_shape(hparams=hparams, batch_size=batch_size)),
                     tf.TensorShape([batch_size]),
                     tf.TensorShape([batch_size, None]),
-                    tf.TensorShape(None),
+                    tf.TensorShape([1, None]),
                     tf.TensorShape(None),
                 ],
             )
         )
-        #print(tokens,prob,"sample_sequence output")
-        return tokens,prob
+        #combine the probabilities and token sequence into one tensor that can be unpacked in gpt_2.generate
+        #I don't think we need to do this but it made more intuitive sense to unpack them together
+        tokens = tf.cast(tokens, tf.float32)
+        out = tf.stack([tokens, prob])
+
+        return out
