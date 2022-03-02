93db5b659a6e46cf950265c97b375e060de8a2cb
Qiu Yuan <qiuyuan463@gmail.com>
2022-03-02 14:07:03 +0000

    Change list size to up to 10

--- locations_to_words/extract_word.py
+++ locations_to_words/extract_word.py
@@ -58,7 +58,7 @@ def postprocess_words(list_of_words):
 def conclude_words(latitude, longitude, radius=0.001):
     # Query limit is 2, so at maximum try one more time for each user
     query_words, _ = set_usage_by_coords(latitude, longitude, radius=radius)
-    if len(query_words) < 3:
+    if len(query_words) < 6:
         query_words, _ = set_usage_by_coords(latitude, longitude, radius=radius * 2)
     with open(word_file, 'rb') as f:
         sample_dict = pickle.load(f)
@@ -70,5 +70,5 @@ def conclude_words(latitude, longitude, radius=0.001):
             dict_words[w] = 1
     dict_words = {k: v for k, v in sorted(dict_words.items(), key=lambda item: item[1])}
     all_words = list(dict_words.keys())
-    all_words = postprocess_words(all_words)[0:3]
+    all_words = postprocess_words(all_words)[:10]
     return all_words
